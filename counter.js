66ef044e8ab28e26f1354d9c2a3d7ae02f0dd50e
roger87 <willy32136@gmail.com>
2020-07-05 11:12:23 +0800

    更改參數

--- counter.js
+++ counter.js
@@ -1,34 +1,38 @@
 $(document).ready(function () {
 
     $(window).scroll(function () {
+
+        $("*").each(function (index, element){
+            var offset = $(this).offset();
+            var top = offset.top;
+            var left = offset.left;
+            
+            // console.log("元素上方:"+ top);
+    
+            var windowTop =  $(window).scrollTop();
+    
+            // console.log("視窗上方:"+ windowTop);
+    
+            var number = $(this).attr("data-counter-number");
+            var time = $(this).attr("data-counter-time");
+            var offset = $(this).attr("data-counter-offset");
+    
+            // console.log("數字" + number);
+            // console.log("時間" + time);
+            // console.log("位移" + offset);
+    
+            if (windowTop > top - offset) {
+               $(this).animate({
+                   num : number
+               },{
+                   duration: parseInt(time),
+                   step: (now) => {
+                       $(this).text(Math.floor(now));
+                   }
+               })
+            }
+        });
         
-        var offset = $("#test").offset();
-        var top = offset.top;
-        var left = offset.left;
-        
-        // console.log("元素上方:"+ top);
 
-        var windowTop =  $(window).scrollTop();
-
-        // console.log("視窗上方:"+ windowTop);
-
-        var number = $("#test").attr("data-counter-number");
-        var time = $("#test").attr("data-counter-time");
-        var offset = $("#test").attr("data-counter-offset");
-
-        // console.log("數字" + number);
-        // console.log("時間" + time);
-        // console.log("位移" + offset);
-
-        if (windowTop > top - offset) {
-           $("#test").animate({
-               num : number
-           },{
-               duration: parseInt(time),
-               step: (now) => {
-                   $("#test").text(Math.floor(now));
-               }
-           })
-        }
     });
 });
